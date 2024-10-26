e4f028f05eb850c43aa2a4855957ce354df74852
Sasoriman <pavelbomberov@gmail.com>
2024-10-26 17:39:16 +0300

    feat: hw add a3+ done

--- jshomework/hwadd/hw1_a3/index.js
+++ jshomework/hwadd/hw1_a3/index.js
@@ -1,16 +1,28 @@
-const isPol = (str) => {
-    for (let i = 0; i < str.length; i++) {
-        if (str[i] === )
+const deleteSymbols = word => {
+    let noSymbolWord = '';
+    const symbols = '~ ! @ # $ % ^ & * ( ) _ - + = { } [ ] | \ : ; " \' < > , . ? / § ± « » × ÷ µ ¢ £ ¥ © ® ™ ℅ ℗ №';
+    for (let i = 0; i < word.length; i++) {
+        if (!symbols.includes(word[i])) noSymbolWord += word[i];
     }
-    if (str.length % 2) {
-        for (let i = 0; i < str.length / 2; i++) {
-
-            if (str[i] !== str[(str.length - i)]) return false;
-        }
-        return true
-    }
-
-
+    return (noSymbolWord === '') ? 0 : noSymbolWord.toLowerCase();
 }
 
-console.log(isPol('aaa'))
+const equalLetter = (letter1, letter2) => {
+    switch (true) {
+        case (letter1 === 'е' || letter1 === 'ё') && (letter2 === 'е' || letter2 === 'ё'): return true;
+        case (letter1 === 'ь' || letter1 === 'ъ') && (letter2 === 'ъ' || letter2 === 'ь'): return true;
+        case letter1 === letter2: return true;
+        default: return false;
+    }
+}
+
+const isPol = (str) => {
+    if (typeof (str) !== 'string') return 0;
+    str = deleteSymbols(str);
+    if (!str) return 0;
+    for (let i = 0; i < Math.floor((str.length - 1) / 2); i++) {
+        if (!equalLetter(str[i], str[((str.length - 1) - i)])) return false
+    }
+    return true
+}
+
