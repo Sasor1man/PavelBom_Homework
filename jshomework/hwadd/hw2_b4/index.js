616c47162a7ba13313f637116a6e18f631580777
PavelBomberov <pavelbomberov@gmail.com>
2024-11-08 15:44:33 +0300

    init: hw b4+ done

---
+++ jshomework/hwadd/hw2_b4/index.js
@@ -0,0 +1,33 @@
+const buildWrapper = tag => (text, atribute) => {
+    if (Array.isArray(text) && text.length <= 2 && typeof (text[0]) === 'string' && typeof (text[1]) === 'object') {
+        atribute = text[1];
+        text = text[0];
+    }
+
+    if (typeof (tag) !== 'string' || typeof (text) !== 'string') return 0;
+    const symbolsUni = {
+        '&': '&amp',
+        '<': '&lt',
+        '>': '&gt',
+        '"': '&quot',
+        "'": '&apos',
+    };
+    const symbols = `&<>'"`;
+    const toUnicode = symbol => `${symbolsUni[symbol]};`;
+    const translateString = str => Array.from(str).map((curLetter) => symbols.includes(curLetter) ? toUnicode(curLetter) : curLetter).join('');
+    const atributeString = atribute => {
+        let result = '';
+        for (key in atribute) {
+            result += ` ${key}='${atribute[key]}'`
+        }
+        return result
+    }
+    for (let key in atribute) {
+        atribute[key] = translateString(atribute[key])
+    }
+    return `<${tag}${atributeString(atribute)}>${translateString(text)}</${tag}>`
+}
+
+const wrapP = buildWrapper('P');
+const wrapH1 = buildWrapper('H1');
+
---
+++ jshomework/hwadd/hw2_b4/test.js
@@ -0,0 +1,88 @@
+function testP(testArr) {
+    for (let i = 0; i < testArr.length; i++) {
+        wrapP(testArr[i].value) === testArr[i].expect ? console.log(`example №${i + 1} good`) : console.log(`example №${i + 1} error`)
+    }
+}
+function testH1(testArr) {
+    for (let i = 0; i < testArr.length; i++) {
+        wrapH1(testArr[i].value) === testArr[i].expect ? console.log(`example №${i + 1} good`) : console.log(`example №${i + 1} error`)
+    }
+}
+
+let aa; // undefined
+const b = null;
+
+const testValuesforP = [
+    {
+        value: "Однажды в студёную зимнюю пору",
+        expect: "<P>Однажды в студёную зимнюю пору</P>"
+    },
+    {
+        value: "Вкусные M&M's",
+        expect: "<P>Вкусные M&amp;M&apos;s</P>"
+    },
+    {
+        value: ["Однажды в студёную зимнюю пору", { lang: "ru" }],
+        expect: "<P lang='ru'>Однажды в студёную зимнюю пору</P>"
+    },
+    {
+        value: "Однажды в <студёную> зимнюю пору",
+        expect: "<P>Однажды в &lt;студёную&gt; зимнюю пору</P>"
+    },
+    {
+        value: 1234,
+        expect: 0
+    },
+    {
+        value: aa,
+        expect: 0
+    },
+    {
+        value: b,
+        expect: 0
+    },
+    {
+        value: ['sos', 101, { a: 102 }, aa, b],
+        expect: 0
+    },
+    {
+        value: ['sos', { a: 102 }, 101, aa, b],
+        expect: 0
+    }
+]
+
+const testValuesforH1 = [
+    {
+        value: "СТИХИ",
+        expect: "<H1>СТИХИ</H1>"
+    },
+    {
+        value: ["СТИХИ", { align: "center", title: "M&M's" }],
+        expect: "<H1 align='center' title='M&amp;M&apos;s'>СТИХИ</H1>"
+    },
+    {
+        value: 1234,
+        expect: 0
+    },
+    {
+        value: aa,
+        expect: 0
+    },
+    {
+        value: b,
+        expect: 0
+    },
+    {
+        value: ['sos', 101, { a: 102 }, aa, b],
+        expect: 0
+    },
+    {
+        value: ['sos', { a: 102 }, 101, aa, b],
+        expect: 0
+    }
+]
+
+console.log('WrapP test')
+console.log(testP(testValuesforP));
+console.log('WrapH1 test')
+console.log(testH1(testValuesforH1));
