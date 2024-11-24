03b1a0f99bb38611841f14f7b2b9f04ec4847b2e
PavelBomberov <pavelbomberov@gmail.com>
2024-11-24 16:36:45 +0300

    feat: hw 27 done

---
+++ jshomework/hw27_valid/script.js
@@ -0,0 +1,83 @@
+const inputs = document.querySelectorAll("input");
+const textarea = document.querySelector("textarea");
+const button = document.querySelector("button");
+
+const isEmpty = value => !(value === '')
+
+const checkUrl = value => {
+    const reg = /^[a-z0-9]+(\.[a-z0-9]+)+$/i;
+    return reg.test(value)
+}
+
+const checkMail = value => {
+    const reg = /\w+@[\w]\.[a-z]/i;
+    return reg.test(value)
+}
+
+const checkText = tag => {
+    switch (tag.name) {
+        case 'siteUrl': return checkUrl(tag.value);
+        case 'mail': return checkMail(tag.value);
+        default: return isEmpty(tag.value)
+    }
+}
+
+const checkDate = date => {
+    const arrayDate = date.split('-');
+    const now = new Date();
+    const currectDate = new Date(arrayDate[0], arrayDate[1] + 1, arrayDate[2]);
+    return currectDate >= now
+}
+
+const checkInput = input => {
+    switch (input.type) {
+        case 'text': return checkText(input);
+        case 'number': return isEmpty(input.value);
+        case 'date': return checkDate(input.value);
+        default: return isEmpty(input.value)
+    }
+}
+
+const addEvent = input => {
+    const blurFunc = e => {
+        const element = e.target;
+        checkInput(element) ? element.classList.add("correct") : element.classList.add("incorrect");
+        element.removeEventListener('blur', blurFunc);
+    }
+    input.addEventListener('focus', e => {
+        const element = e.target;
+        element.classList.remove('correct', 'incorrect');
+        element.addEventListener('blur', blurFunc)
+    });
+}
+
+const checkCorrect = () => {
+    let a = 0;
+    inputs.forEach(e => {
+        if (!checkInput(e)) {
+            if (!e.classList.contains('incorrect')) e.classList.add("incorrect");
+            a++;
+        }
+    })
+    if (!checkInput(textarea)) {
+        if (!textarea.classList.contains('incorrect')) textarea.classList.add("incorrect");
+        a++;
+    }
+    return a
+}
+
+const pseudoSubmit = e => {
+    if (checkCorrect()) e.preventDefault();
+}
+
+inputs.forEach(e => addEvent(e));
+
+addEvent(textarea);
+
+button.addEventListener('click', pseudoSubmit)
+
+
+
+
+
+
