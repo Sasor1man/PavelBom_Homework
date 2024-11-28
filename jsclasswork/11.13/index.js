86fbcce12c433118c4dffba817b1e598a5d1f558
PavelBomberov <pavelbomberov@gmail.com>
2024-11-28 20:11:17 +0300

    init: year sort

--- jsclasswork/11.13/index.js
+++ jsclasswork/11.13/index.js
@@ -6,64 +6,60 @@ const cars = [
         price: 120000,
         year: 2006,
         transmissionType: 'automatic'
-    },{
+    }, {
         brand: 'Mercedes',
         model: 'AMG GT',
         imgUrl: 'photos/images.jpeg',
         price: 100000,
         year: 2020,
         transmissionType: 'automatic'
-    },{
-        brand: 'Mercedes',
-        model: 'AMG GT',
-        imgUrl: 'photos/images.jpeg',
-        price: 10000,
-        year: 2020,
+    }, {
+        brand: 'Audi',
+        model: 'Q7',
+        imgUrl: 'photos/q7.jpg',
+        price: 45000,
+        year: 2017,
         transmissionType: 'automatic'
-    },,{
-        brand: 'Mercedes',
-        model: 'AMG GT',
-        imgUrl: 'photos/images.jpeg',
-        price: 150000,
-        year: 2020,
+    }, {
+        brand: 'Audi',
+        model: 'R8',
+        imgUrl: 'photos/r8.jpg',
+        price: 55000,
+        year: 2008,
+        transmissionType: 'robot'
+    }, {
+        brand: 'Mitsubishi',
+        model: 'EVO X',
+        imgUrl: 'photos/evox.jpg',
+        price: 25000,
+        year: 2012,
         transmissionType: 'automatic'
-    },{
-        brand: 'Mercedes',
-        model: 'AMG GT',
-        imgUrl: 'photos/images.jpeg',
-        price: 300000,
-        year: 2020,
+    }, {
+        brand: 'Mitsubishi',
+        model: 'Xpander',
+        imgUrl: 'photos/expander.jpg',
+        price: 22000,
+        year: 2022,
         transmissionType: 'automatic'
-    }
-    ,{
-        brand: 'Mercedes',
-        model: 'AMG GT',
-        imgUrl: 'photos/images.jpeg',
-        price: 340000,
-        year: 2020,
-        transmissionType: 'automatic'
-    },{
-        brand: 'Mercedes',
-        model: 'AMG GT',
-        imgUrl: 'photos/images.jpeg',
-        price: 300000,
-        year: 2020,
-        transmissionType: 'automatic'
-    }
+    },
 ];
 
 const filterByPriceLabel = document.querySelector('[for=filterByPrice]');
 const filterByPriceInput = document.getElementById('filterByPrice');
 const sortUp = document.querySelector('.sortUp');
 const sortDown = document.querySelector('.sortDown');
+const sortUpYear = document.getElementById('sortUpYear');
+const sortDownYear = document.getElementById('sortDownYear');
+const checkboxes = document.querySelectorAll('[type=checkbox]')
+
 
 const addCar = () => {
     const inputs = document.querySelectorAll('input[name]');
     const select = document.querySelector('select');
-    
+
     const car = {};
-    
-    for(const inp of inputs) {
+
+    for (const inp of inputs) {
         car[inp.name] = inp.value
     }
     cars.push(car)
@@ -92,21 +88,36 @@ render(cars)
 
 filterByPriceInput.addEventListener('input', e => {
     filterByPriceLabel.innerHTML = `${e.target.value} $`;
-
     const filterdCars = cars.filter(car => parseInt(car.price) <= parseInt(e.target.value));
-
     render(filterdCars)
 })
 
+const checkboxFunc = e => {
+
+}
+
 sortUp.addEventListener('click', () => {
-    cars.sort(a,b => a.price - b.price)
+    cars.sort((a, b) => a.price - b.price)
 
     render(cars)
 })
 
 sortDown.addEventListener('click', () => {
-    cars.sort(a,b => b.price - a.price)
+    cars.sort((a, b) => b.price - a.price)
 
     render(cars)
 })
 
+sortUpYear.addEventListener('click', () => {
+    cars.sort((a, b) => a.year - b.year)
+
+    render(cars)
+})
+
+sortDownYear.addEventListener('click', () => {
+    cars.sort((a, b) => b.year - a.year)
+
+    render(cars)
+})
+
+// checkboxes.forEach(input => input.addEventListener('click', checkboxFunc))
